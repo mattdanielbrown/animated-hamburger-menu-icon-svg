5e9eb30fc46c9e37a12bd8e138f6bae523ddcbb7
Matt Daniel Brown <developer@mattdanielbrown.com>
2020-08-20 20:03:05 -0500

    Rearranged Codebase and Added, renamed, deleted necessary files

--- app/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function main() {
-  console.log('Main Function Started');
-}
-window.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOM fully loaded and parsed');
-  main();
-});
